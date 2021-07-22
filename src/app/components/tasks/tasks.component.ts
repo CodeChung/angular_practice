9fccbf46ae7d3e6d5b3af013a6a2bf58a0bb2ce1
Harry Chung <harrychung@Harrys-MBP.home>
2021-07-21 21:14:04 -0700

    add task yo

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -21,4 +21,7 @@ export class TasksComponent implements OnInit {
     task.reminder = !task.reminder;
     this.taskService.updateTaskReminder(task).subscribe(() => {});
   }
+  addTask(task: Task): void {
+    this.taskService.addTask(task).subscribe(task => this.tasks.push(task));
+  }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {Observable,} from 'rxjs';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Task} from '../Task';
+import { Observable, } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Task } from '../Task';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -28,5 +28,11 @@ export class TaskService {
   updateTaskReminder(task: Task): Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`
     return this.http.put<Task>(url, task, httpOptions);
+  }
+
+  addTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}`
+    console.log("MI TASK SENOR", task)
+    return this.http.post<Task>(url, task, httpOptions);
   }
 }
